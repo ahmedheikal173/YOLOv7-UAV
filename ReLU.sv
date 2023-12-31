e008c1eb2498864ef4321046ebd10e0602571b13
Hyysham <hisham.tarik@gmail.com>
2023-12-31 18:30:19 +0200

    Update ReLU.sv

--- ReLU.sv
+++ ReLU.sv
@@ -4,20 +4,36 @@ parameter datawidth=8;
 parameter N=320;
 parameter depth=3;
 
-input logic [datawidth-1:0] in [N-1:0] [N-1:0] [depth-1:0];
+input [datawidth-1:0] in [N-1:0][N-1:0][depth-1:0];
 input rstn,clk,en;
 
-output logic [datawidth-1:0] out [N-1:0] [N-1:0] [depth-1:0];
+output reg [datawidth-1:0] out [N-1:0][N-1:0][depth-1:0];
+
+integer i,j,k;
 
   always@(posedge clk or negedge rstn) begin
 
 	if(!rstn) begin
-		out<=0;
+		for (i=0;i<N;i=i+1) begin
+			for (i=0;i<N;i=i+1) begin
+				for (i=0;i<N;i=i+1) begin
+					out[i][j][k]<=0;
+				end
+			end
+		end
 	end
 	else if (en) begin
-		out<=in;
+		for (i=0;i<N;i=i+1) begin
+			for (i=0;i<N;i=i+1) begin
+				for (i=0;i<N;i=i+1) begin
+					out[i][j][k]<=in[i][j][k];
+				end
+			end
+		end
+		//out=in;
 	end
 
   end
 
+//assign out=in;
 endmodule
