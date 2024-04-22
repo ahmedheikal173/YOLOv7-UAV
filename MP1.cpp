2fc4aa0cd91bd9f783e88de2ecd1fabe99746979
MonaTarek12 <120265391+MonaTarek12@users.noreply.github.com>
2024-04-22 13:02:58 +0200

    Delete MP1.cpp

--- MP1.cpp
+++
@@ -1,6 +0,0 @@
-#include "arch.h"
-void MP1(fptype input[widthMP1][lengthMP1][features_numberMP1],fptype output_MP1[outputWidthMP1][outputLengthMP1][features_numberMP1],fptype weights[features_numberMP1])
-{
-	MaxPoolingMP1(input,output_MP1);
-	convMP1(output_MP1,weights,output_MP1);
-}
