f736276007f20ce0053bd00b98b422d1815e3fac
Hyysham <hisham.tarik@gmail.com>
2024-06-10 00:35:05 +0300

    .

---
+++ memory_architecture_and_optimization/various issues/2-Memory_Organization2/m2/m2/solution1/sim/wrapc_pc/write_mem_rnd.hpp
@@ -0,0 +1,26 @@
+#ifndef __WRITE_MEM_RND__
+#define __WRITE_MEM_RND__
+
+#include "../include/ap_int.h"
+
+template <typename T, int N>
+void interleave_mem_rnd<T, N>::write_rnd(ap_uint<ADDRESS_BITWIDTH> i,
+                                 T x_in[N]) {
+                                  
+#pragma HLS BIND_STORAGE variable=x_in type=RAM_T2P impl=BRAM
+
+  T tmp = x_in[i];
+  switch (i % 3) {
+    case 0:
+      x0[i / 3] = tmp;
+      break;
+    case 1:
+      x1[i / 3] = tmp;
+      break;
+    case 2:
+      x2[i / 3] = tmp;
+      break;
+  }
+}
+
+#endif
---
+++ memory_architecture_and_optimization/various issues/2-Memory_Organization2/src/interleave_manual_rnd.cpp
@@ -0,0 +1,32 @@
+#include "./interleave.h"
+#include "./interleave_mem_rnd.hpp"
+
+void interleave_manual_rnd(ap_int<8> x_in[NUM_WORDS],
+                           ap_int<8> y[NUM_WORDS / 3], bool load) {
+// #pragma HLS disaggregate variable = x_in
+// #pragma HLS disaggregate variable = y
+
+
+//#pragma HLS RESOURCE variable=x_in core=RAM_1P_BRAM
+//#pragma HLS BIND_STORAGE variable=x_in type=RAM_T2P impl=BRAM
+#pragma HLS interface mode=BRAM port=x_in //[OPTIONS]
+//#pragma HLS RESOURCE variable=y core=RAM_1P_BRAM
+//#pragma HLS BIND_STORAGE variable=y type=RAM_T2P impl=BRAM
+#pragma HLS interface mode=BRAM port=y //[OPTIONS]
+
+  static interleave_mem_rnd<ap_int<8>, NUM_WORDS> x;
+  
+#pragma HLS BIND_STORAGE variable=x type=RAM_T2P impl=BRAM
+  int idx = 0;
+
+  if (load)
+LOAD:
+    for (int i = 0; i < NUM_WORDS; i += 1)
+#pragma HLS PIPELINE II=1
+    	x.write_rnd(i, x_in);
+  else
+WRITE:
+    for (int i = 0; i < NUM_WORDS; i += 3)
+#pragma HLS PIPELINE II=1
+      y[idx++] = x.read_rnd(i, 0) + x.read_rnd(i, 1) + x.read_rnd(i, 2);
+}
